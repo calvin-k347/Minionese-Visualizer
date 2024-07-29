45a6273e22ee2b47cbfc80975b21243afdb6fcd6
calvin-k347 <clark.calvink@gmail.com>
2024-07-28 21:45:30 -0400

    implemented rudimentary drag and drop feature Fixed: #2

--- static/script.js
+++ static/script.js
@@ -1,7 +1,21 @@
+const fileUpload = document.getElementById("File_Upload");
+const hiddenFileInput = document.getElementById("hiddenFileInput");
 function fileDropped(){
-    console.log("dropped")
+    console.log("dropped");
 }
 
 function openFileSelect(){
-    document.getElementById("hiddenFileInput").click()
+    document.getElementById("hiddenFileInput").click();
 }
+fileUpload.addEventListener("dragover", function(event){
+    event.preventDefault();
+    console.log("s");
+    fileUpload.classList.add("drag-over");
+});
+
+fileUpload.addEventListener("drop", function(event){
+    event.preventDefault();
+    hiddenFileInput.files = event.dataTransfer.files;
+    console.log("dropped");
+    fileUpload.classList.remove("drag-over");
+});
